ce471d5f23fccd4abb037ebc46f6e2385be1f554
Leejoonsoo524 <may2497@nate.com>
2025-11-18 13:00:44 +0900

    포폴 페이지 디자인완료 내용추가예정

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0)       header.classList.add('scroll');
+    else                          header.classList.remove('scroll');
+});
